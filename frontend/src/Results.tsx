b073a47c20da286bf67c3ed2c2913f7cd9a11b11
Scott Oswald <scottoswald1@gmail.com>
2026-04-24 17:14:40 +0100

    add results page

---
+++ frontend/src/Results.tsx
@@ -0,0 +1,14 @@
+function Results() {
+  return (
+    <div className="container">
+      <h1 className="logo">Halfterm</h1>
+      <p className="tagline">Here's what we found for you</p>
+
+      <div className="results-box">
+        <p className="placeholder">Your results will appear here...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Results
